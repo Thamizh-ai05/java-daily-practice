1879f29af23c9b715c1d7d7c8a518c72272766b5
THAMIZH05 <thamizh.ai05@gmail.com>
2025-08-12 22:20:04 +0530

    OddEven Program

---
+++ src/firstPack/OddEven.java
@@ -0,0 +1,16 @@
+package firstPack;
+
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n%2==0){
+            System.out.print(n+" is Even Number");
+        }
+        else{
+            System.out.print(n+" is Odd Number");
+        }
+    }
+}
