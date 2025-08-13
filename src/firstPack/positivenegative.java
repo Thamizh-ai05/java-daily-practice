137d1c231cec4ca81686203370d53a418aa9b2fd
THAMIZH05 <thamizh.ai05@gmail.com>
2025-08-13 16:58:58 +0530

    Positive_or_Negative

--- src/firstPack/positivenegative.java
+++ src/firstPack/positivenegative.java
@@ -3,15 +3,17 @@ package firstPack;
 import java.util.Scanner;
 
 public class positivenegative {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    if(n==0){
-        System.out.println("The Number is Zero");
-    }
-    else if(n>0){
-        System.out.println(n+" is Positive");
-    }
-    else{
-        System.out.println(n+" is Negative");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n==0){
+            System.out.println("The Number is Zero");
+        }
+        else if(n>0){
+            System.out.println(n+" is Positive");
+        }
+        else{
+            System.out.println(n+" is Negative");
+        }
     }
 }
